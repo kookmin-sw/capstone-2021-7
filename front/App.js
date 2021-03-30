49dc3e3633e9495bd3039ff6a3a094fcbf2a2e23
yumi <ckdballl@naver.com>
2021-03-31 03:26:36 +0900

    recommend page version.1

--- front/App.js
+++ front/App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Recommend from './components/recommend';
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Recommend></Recommend>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
