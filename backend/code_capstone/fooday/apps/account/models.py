db817019d78031217a5ed5995c313007091c8d49
Ubuntu <ubuntu@ip-172-31-4-224.ap-northeast-2.compute.internal>
2021-04-06 15:18:03 +0000

    backend setting- DB구축 및 카테고리 데이터 생성버젼

---
+++ backend/code_capstone/fooday/apps/account/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+from apps.category.models import *
+from apps.store.models import *
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    GENDER = (
+        ('male','male'),
+        ('female','female')
+    )
+    phone = models.CharField(max_length = 150, unique = True ,default = "null")
+    name = models.CharField(max_length = 150, default = "null")
+    gender = models.CharField(max_length = 150, choices = GENDER, default = "null")
+    taste = models.IntegerField(default = 0)
+    price = models.IntegerField(default = 0)
+    amount = models.IntegerField(default = 0)
+    userSmallCategory = models.ManyToManyField(SmallCategory, through = "User_SmallCategory")
+    userStore = models.ManyToManyField(Store, through = "User_Store")
+    
+class User_SmallCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    smallCategory = models.ForeignKey(SmallCategory, on_delete=models.CASCADE)
+    eventType = models.IntegerField(default = 0)
+    eventCount = models.IntegerField(default = 0)
+    timestamp = models.DateTimeField(default = timezone.now)
+     
+class User_Store(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    
+class User_Menu(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default = timezone.now)
+    weather = models.CharField(max_length = 150, default = "null")
+    
+    
+    
+
+    
