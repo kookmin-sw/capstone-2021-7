c3ff0d1337b8967754cf73c8eeb1d9f5d31a9b6a
박종민 <showppark12@naver.com>
2021-05-08 19:28:47 +0900

    회원가입

--- front/api/user-api.js
+++ front/api/user-api.js
@@ -1,6 +1,7 @@
 import { callAPI } from "./api-config";
 
 const postUserInformation = (postData) => {
+    console.log("데이터",postData);
     return callAPI("POST", "user/signup/", postData);
   };
 
